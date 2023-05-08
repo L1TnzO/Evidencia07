e624e18a9f4cc56f3cefe3f8a16462a6118d57a3
L1TnzO <59234471+L1TnzO@users.noreply.github.com>
2023-05-08 02:34:05 -0400

    Add files via upload

---
+++ Calculadora/src/main/java/org/example/Calculadora.java
@@ -0,0 +1,31 @@
+public class Calculadora {
+
+	private int n1;
+	private int n2;
+
+	public Calculadora() {
+		// TODO - implement Calculadora.Calculadora
+		throw new UnsupportedOperationException();
+	}
+
+	public int sumar() {
+		// TODO - implement Calculadora.sumar
+		throw new UnsupportedOperationException();
+	}
+
+	public int multiplicar() {
+		// TODO - implement Calculadora.multiplicar
+		throw new UnsupportedOperationException();
+	}
+
+	public void setN1() {
+		// TODO - implement Calculadora.setN1
+		throw new UnsupportedOperationException();
+	}
+
+	public void setN2() {
+		// TODO - implement Calculadora.setN2
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ Calculadora/src/main/java/org/example/CarroCompra.java
@@ -0,0 +1,25 @@
+public class CarroCompra {
+
+	private int[][] productos;
+
+	public CarroCompra() {
+		// TODO - implement CarroCompra.CarroCompra
+		throw new UnsupportedOperationException();
+	}
+
+	public void calcularTotal() {
+		// TODO - implement CarroCompra.calcularTotal
+		throw new UnsupportedOperationException();
+	}
+
+	public int subTotal() {
+		// TODO - implement CarroCompra.subTotal
+		throw new UnsupportedOperationException();
+	}
+
+	public void mostrarTotal() {
+		// TODO - implement CarroCompra.mostrarTotal
+		throw new UnsupportedOperationException();
+	}
+
+}
